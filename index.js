2cc91ca8d82ef6d440ec9da4106a3be52c807717
Kim Foukaris <kimfoukaris@outlook.com>
2018-07-02 10:13:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function produceDrivingRange(blockRange) {
   return function(start, stop) {
-    diff = Math.abs(stop.slice(0,2) - start.slice(0,2) - blockRange)
+    diff = Math.abs(stop.slice(0,2) - start.slice(0,2)) - blockRange
     if (diff > 0) {
       return diff + ' blocks out of range'
     } else {
